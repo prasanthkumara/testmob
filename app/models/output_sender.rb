38b98d1380e8293e1ba7ec72863dbdb501d1ae42
Dileep Nandanam <dileepsnandanam@gmail.com>
2020-06-17 16:32:48 +0530

    mapping corrected

--- app/models/output_sender.rb
+++ app/models/output_sender.rb
@@ -2,13 +2,14 @@ class OutputSender < ApplicationJob
   O_MAP = {
     1 => "Command Executed Successfully",
     0 => "Command Failed",
-    -1 => "Emergency Stop pressed / Door open / Torque off Invalid Parameters",
-    -2 => "Invalid API",
-    -3 => "Outside Workspace",
-    -4 => "Invalid Path",
-    -5 => "Manipulator is not Homed",
-    -6 => "Wrong number of parameters",
-    -7 => "Parameter data type is wrong"
+    -2 => "Emergency Stop pressed / Door open / Torque off"
+    -4 => "Invalid Parameters",
+    -5 => "Invalid API",
+    -6 => "Outside Workspace",
+    -7 => "Invalid Path",
+    -8 => "Manipulator is not Homed",
+    -9 => "Wrong number of parameters",
+    -10 => "Parameter data type is wrong"
   }
   def perform(user_id, line, output)
     @user = User.find(user_id)
