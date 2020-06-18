f714aa05230d2c7c5047b6af143ea7b58ace927f
Dileep Nandanam <dileepsnandanam@gmail.com>
2020-06-18 09:34:35 +0530

    fix

--- app/controllers/app_configs_controller.rb
+++ app/controllers/app_configs_controller.rb
@@ -1,6 +1,8 @@
 class AppConfigsController < ApplicationController
   def update
-    redirect_to root_path unless current_user.usertype == 'platform' and return
+    unless current_user.usertype == 'platform'
+      redirect_to root_path and return
+    end
     AppConfig.find(params[:id]).update params.require(:app_config).permit(:value)
     redirect_to configure_tests_path
   end
