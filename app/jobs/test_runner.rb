038787b490589de9daca40344e0c63d71a371f32
Dileep Nandanam <dileepsnandanam@gmail.com>
2020-06-18 12:08:58 +0530

    telnet

--- app/jobs/test_runner.rb
+++ app/jobs/test_runner.rb
@@ -1,15 +1,34 @@
 class TestRunner < ApplicationJob
   def perform(test_id)
+    if false
+      require 'socket'
+      sock = TCPSocket.new(AppConfig.where(name: 'host').first.value, AppConfig.where(name: 'port').first.value.to_i)    
 
-    require 'socket'
-    sock = TCPSocket.new(AppConfig.where(name: 'host').first.value, AppConfig.where(name: 'port').first.value.to_i)    
-
-    @test = Test.find(test_id)
-    @test.code.split(/\n/).each do |line|
-      sock.write line
-      output = sock.read(1024)
-      OutputSender.perform_later(@test.user_id, line, output)
+      @test = Test.find(test_id)
+      @test.code.split(/\n/).each do |line|
+        sock.write line
+        output = sock.read(1024)
+        OutputSender.perform_later(@test.user_id, line, output)
+      end
+      sock.close
     end
-    sock.close
+
+
+    require 'net/telnet'
+
+    server = Net::Telnet::new('Host' => AppConfig.where(name: 'host').first.value,
+                          'Port' => AppConfig.where(name: 'port').first.value.to_i,
+                          'Telnetmode' => false)
+    
+    @test = Test.find(test_id)
+    fork do
+      @test.code.split(/\n/).each do |line|
+        server.puts(line)
+        server.waitfor(/./) do |data|
+          OutputSender.perform_later(@test.user_id, line, data)
+        end
+      end
+    end
+    
   end
 end
