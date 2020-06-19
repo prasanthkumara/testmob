5d59ef5722c82183db964c124ec3641e62d297ca
Dileep Nandanam <dileepsnandanam@gmail.com>
2020-06-19 19:52:00 +0530

    only quaco tcp

--- app/jobs/test_runner.rb
+++ app/jobs/test_runner.rb
@@ -3,9 +3,15 @@ class TestRunner < ApplicationJob
 
   def perform(test_id)    
     @test = Test.find(test_id)
-  
+    if Quaco.connection.nil?
+      Quaco.connect('tcp')
+    end
     @test.code.split(/\n/).each do |line|
       Quaco.execute(@test.user_id, line) if line.present?
+    end
+
+    unless Quaco.connection.nil?
+      Quaco.disconnect
     end
   end
 end
