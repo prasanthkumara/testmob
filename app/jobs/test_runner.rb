dc6cbf1b9fea019ae67473d75f1802e08bf69af6
Dileep Nandanam <dileepsnandanam@gmail.com>
2020-06-28 11:30:56 +0530

    removing debug

--- app/jobs/test_runner.rb
+++ app/jobs/test_runner.rb
@@ -2,7 +2,6 @@ class TestRunner < ApplicationJob
   queue_as :default
 
   def perform(test_id) 
-    OutputSender.perform_later(1, 'line', '1')   
     @test = Test.find(test_id)
     @test.code.split(/\n/).each do |line|
       Quaco.execute(@test.user_id, line) if line.present?
